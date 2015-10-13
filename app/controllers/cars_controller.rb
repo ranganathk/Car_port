40a5223975d47e173a8a98e1820a984ac0ac7fc8
Ranganath K <ranganathkomandur@gmail.com>
2015-10-13 18:10:30 +0530

    add external file readability

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,14 @@
 class CarsController < ApplicationController
   def index
   end
+
+  def show
+    file = File.join(Rails.root, 'app', 'assets', 'other', 'ranganath_resume.pdf')
+    f = File.open(file, "r")
+    f.each_line do |line|
+      puts line
+    end
+    f.close
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'cars#index'
+  get '/show' => 'cars#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
